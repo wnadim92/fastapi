a1edcf3033ac8f722b3e2d045442b8e7e4fd62a4
Samuel Nadim <wnadim2992@gmail.com>
2025-07-23 00:25:28 -0400

    update

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -1,1 +1,12 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
+from .. import schemas, database, models, oauth2  
+
+router = APIRouter(
+    prefix="/api/vi/vote",
+    tags=["Vote"]
+)
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+def vote(vote: schemas.Vote, db: Session = Depends(database.get_db), current_user: int = Depends(oauth2.get_current_user)):
+    if (vote.dir == 1):
+        db.query(models.Vote).filter(models.Vote.post_id == vote.post_id, models.Vote.user_id == current_user.id)
--- app/schemas.py
+++ app/schemas.py
@@ -2,6 +2,8 @@ from pydantic import BaseModel, EmailStr
 from datetime import datetime
 from typing import Optional
 
+from pydantic.types import conint
+
 class Post(BaseModel):
     title: str
     content: str
@@ -44,3 +46,8 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     id: Optional[str] = None
+
+class Vote(BaseModel):
+    post_id: int
+    dir: conint(le=1)
+     
