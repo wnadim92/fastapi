0a0566444319af7133b90aa3829df142602a785c
Samuel Nadim <wnadim2992@gmail.com>
2025-07-23 00:51:20 -0400

    wire up route

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from . import models
 from .database import engine
-from . routers import posts, users, auth
+from . routers import posts, users, auth, vote
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -10,6 +10,7 @@ app = FastAPI()
 app.include_router(posts.router)
 app.include_router(users.router)
 app.include_router(auth.router)
+app.include_router(vote.router)
 
 @app.get("/")
 def root():
