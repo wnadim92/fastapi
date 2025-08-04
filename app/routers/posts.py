588709557714cbaa81c794053308645174aff768
Samuel Nadim <wnadim2992@gmail.com>
2025-08-03 21:29:33 -0400

    update get posts

--- app/routers/posts.py
+++ app/routers/posts.py
@@ -3,18 +3,20 @@ from sqlalchemy.orm import Session
 from .. import models, schemas, utils, oauth2
 from .. database import engine, get_db
 from typing import List, Optional
+from sqlalchemy import func
 
 router = APIRouter(
     prefix="/api/v1/posts",
     tags=["Posts"]
 )
 
-@router.get("/", response_model=List[schemas.Post]) 
+@router.get("/", response_model=List[schemas.PostOut]) 
 def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), limit: int = 10, skip: int = 0, search: Optional[str] = ""):
     # cursor.execute(""" SELECT * FROM posts """)
     # posts = cursor.fetchall()
     posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
-    return posts
+    results = db.query(models.Post, func.count(models.Vote.post_id).label("vote")).join(models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).all()
+    return [{"Post": post, "votes": votes} for post, votes in results]
 
 @router.get("/latest", response_model=schemas.Post)
 def latest_post(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
