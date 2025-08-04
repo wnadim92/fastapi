e5dd948d8458c1c3848f2bf26cd5639038d332df
Samuel Nadim <wnadim2992@gmail.com>
2025-08-03 21:33:34 -0400

    update filter

--- app/routers/posts.py
+++ app/routers/posts.py
@@ -14,8 +14,7 @@ router = APIRouter(
 def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), limit: int = 10, skip: int = 0, search: Optional[str] = ""):
     # cursor.execute(""" SELECT * FROM posts """)
     # posts = cursor.fetchall()
-    posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
-    results = db.query(models.Post, func.count(models.Vote.post_id).label("vote")).join(models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).all()
+    results = db.query(models.Post, func.count(models.Vote.post_id).label("vote")).join(models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
     return [{"Post": post, "votes": votes} for post, votes in results]
 
 @router.get("/latest", response_model=schemas.Post)
