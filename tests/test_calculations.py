a909b5c2b23af17e4b361a64f48f85e28ec195dd
Samuel Nadim <wnadim2992@gmail.com>
2025-08-23 21:51:59 -0400

    add fixture into pytest test testing

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,6 +1,15 @@
 import pytest
 from app.calculations import add, subtract, multiply, divide, BankAccount
 
+@pytest.fixture
+def zero_bank_account():
+    print("creating empty bank account")
+    return BankAccount()
+
+@pytest.fixture
+def bank_account():
+    return BankAccount(50)
+
 @pytest.mark.parametrize("num1, num2, expected", [
     (5, 3, 8),
     (10, 5, 15),
@@ -19,25 +28,21 @@ def test_multiply():
 def test_divide():
     assert divide(6, 3) == 2
 
-def test_bank_set_initial_amount():
-    bank_account = BankAccount(50)
+def test_bank_default_amount(zero_bank_account):
+    print("testing my bank account")
+    assert zero_bank_account.balance == 0
+
+def test_bank_set_initial_amount(bank_account):
     assert bank_account.balance == 50
 
-def test_bank_default_amount():
-    bank_account = BankAccount()
-    assert bank_account.balance == 0
-
-def test_withdraw():
-    bank_account = BankAccount(50)
+def test_withdraw(bank_account):
     bank_account.withdraw(20)
     assert bank_account.balance == 30
 
-def test_deposit():
-    bank_account = BankAccount(50)
+def test_deposit(bank_account):
     bank_account.deposit(30)
     assert bank_account.balance == 80
 
-def test_collect_interest():
-    bank_account = BankAccount(50)
+def test_collect_interest(bank_account):
     bank_account.collect_interest()
     assert round(bank_account.balance, 8) == 55
