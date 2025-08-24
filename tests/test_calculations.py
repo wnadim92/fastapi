305ee66abbebd7adfe8493f83de30dcd82bda8b2
Samuel Nadim <wnadim2992@gmail.com>
2025-08-24 05:12:12 +0000

    add decorator to testing

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -47,7 +47,12 @@ def test_collect_interest(bank_account):
     bank_account.collect_interest()
     assert round(bank_account.balance, 8) == 55
 
-def test_bank_transaction(zero_bank_account):
-    zero_bank_account.deposit(200)
-    zero_bank_account.withdraw(100)
-    assert zero_bank_account.balance == 100
+@pytest.mark.parametrize("deposited, withdrew, expected", [
+    (200, 100, 100),
+    (50, 10, 40),
+    (1200, 200, 1000)
+])
+def test_bank_transaction(zero_bank_account, deposited, withdrew, expected):
+    zero_bank_account.deposit(deposited)
+    zero_bank_account.withdraw(withdrew)
+    assert zero_bank_account.balance == expected
