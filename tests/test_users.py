fd55715255965ea6b26930a32cd1c9caa8aa92a5
Samuel Nadim <wnadim2992@gmail.com>
2025-08-26 02:00:23 +0000

    update test to include test db for path operations

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,4 +1,5 @@
 from fastapi.testclient import TestClient
+import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
@@ -8,14 +9,14 @@ from dotenv import load_dotenv
 import os
 from app.config import settings
 from app.database import get_db
+from app.database import Base
  
-
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}-test"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+Base.metadata.create_all(bind=engine)
 
 def override_get_db():
     db = TestingSessionLocal()
@@ -26,16 +27,20 @@ def override_get_db():
 
 app.dependency_overrides[get_db] = override_get_db
 
-client = TestClient(app)
+@pytest.fixture
+def client():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    yield TestClient(app)
 
-def test_root():
+def test_root(client):
     res = client.get("/")
     print(res.json().get("message"))
     assert res.json().get("message") == "Peace, hello world!"
     assert res.status_code == 200
 
-def test_create_user():
-    res = client.post("/api/v1/users/", json={"email": "sam@mydomain.com", "password": "password123"})
+def test_create_user(client):
+    res = client.post("/api/v1/users/", json={"email": "sam2@mydomain.com", "password": "password123"})
     new_user = schemas.UserOut(**res.json())
-    assert new_user.email == "sam@mydomain.com"
+    assert new_user.email == "sam2@mydomain.com"
     assert res.status_code == 201
