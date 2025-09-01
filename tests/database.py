4f6533e727be5bc700328ad1738d464add765b2e
Samuel Nadim <wnadim2992@gmail.com>
2025-09-01 16:49:52 +0000

    add testing suite

--- tests/database.py
+++ tests/database.py
@@ -4,19 +4,25 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from app.main import app
-from dotenv import load_dotenv
-import os
+
 from app.config import settings
 from app.database import get_db
 from app.database import Base
- 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}-test"
+from alembic import command
+
+
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:password123@localhost:5432/fastapi_test'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
+
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+TestingSessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine)
 
-@pytest.fixture
-def session(scope="function"):
+
+@pytest.fixture()
+def session():
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
     db = TestingSessionLocal()
@@ -25,9 +31,11 @@ def session(scope="function"):
     finally:
         db.close()
 
-@pytest.fixture(scope="function")
+
+@pytest.fixture()
 def client(session):
     def override_get_db():
+
         try:
             yield session
         finally:
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,31 +1,49 @@
-from app import schemas
-from .database import client, session
 import pytest
 from jose import jwt
+from app import schemas
+
 from app.config import settings
 
-@pytest.fixture
-def test_user(client):
-    user_data = {"email": "sam1@mydomain.com", "password": "password123"}
-    res = client.post("/api/v1/users/", json=user_data)
-    assert res.status_code == 201
-    print(res.json())
-    new_user = res.json()
-    new_user['password'] = user_data['password']
-    return new_user
+
+# def test_root(client):
+
+#     res = client.get("/")
+#     print(res.json().get('message'))
+#     assert res.json().get('message') == 'Hello World'
+#     assert res.status_code == 200
+
 
 def test_create_user(client):
-    res = client.post("/api/v1/users/", json={"email": "sam2@mydomain.com", "password": "password123"})
+    res = client.post(
+        "/api/v1/users/", json={"email": "hello12345@mydomain.com", "password": "password123"})
+
     new_user = schemas.UserOut(**res.json())
-    assert new_user.email == "sam2@mydomain.com"
+    assert new_user.email == "hello12345@mydomain.com"
     assert res.status_code == 201
 
-def test_login_user(client, test_user):
-    res = client.post("/api/v1/login/", data={"username": test_user['email'], "password": test_user['password']})
+
+def test_login_user(test_user, client):
+    res = client.post(
+        "/api/v1/login", data={"username": test_user['email'], "password": test_user['password']})
     login_res = schemas.Token(**res.json())
-    payload = jwt.decode(login_res.access_token, settings.secret_key, algorithms=[settings.algorithm])
+    payload = jwt.decode(login_res.access_token,
+                         settings.secret_key, algorithms=[settings.algorithm])
     id = payload.get("user_id")
     assert id == test_user['id']
     assert login_res.token_type == "bearer"
     assert res.status_code == 200
 
+
+@pytest.mark.parametrize("email, password, status_code", [
+    ('wrongemail@mydomain.com', 'password123', 403),
+    ('sam@mydomain.com', 'wrongpassword', 403),
+    ('wrongemail@mydomain.com', 'wrongpassword', 403),
+    (None, 'password123', 403),
+    ('sam@mydomain.com', None, 403)
+])
+def test_incorrect_login(test_user, client, email, password, status_code):
+    res = client.post(
+        "/api/v1/login", data={"username": email, "password": password})
+
+    assert res.status_code == status_code
+    # assert res.json().get('detail') == 'Invalid Credentials'
